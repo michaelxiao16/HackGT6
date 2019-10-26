7b6360071f3ce63aa9593a93f74e5bf816383bc4
Ryan Moazzami <ryan.moazz@gmail.com>
2019-10-26 12:12:56 -0400

    classes

--- CanvasDataSource.py
+++ CanvasDataSource.py
@@ -4,9 +4,17 @@ import requests
 
 ACCESS_TOKEN = '7~fxv2FzexxnfWEu2MzAYVnxlzRVvDFZnvzqhTppuhMO8i8xdZN0THcq5yZfSJmHL3'
 
+
 def getClasses():
     auth = requests.get(f"https://canvas.instructure.com/api/v1/courses?access_token={ACCESS_TOKEN}")
     return auth.json()
+
+
+def getAssignments(user_id, course_id):
+    assignments = requests.get(f"https://canvas.instructure.com/api/v1/users/{user_id}}/courses/{course_id}/assignments")
+    return assignments.json()
+
+
 
 
 
