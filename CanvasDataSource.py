7ef952b961f39cad0b2cbf9376ed1dc4f75fdf12
Ryan Miles <ryanm1114@gmail.com>
2019-10-26 13:03:01 -0400

    Fix code

--- CanvasDataSource.py
+++ CanvasDataSource.py
@@ -1,9 +1,7 @@
 ## Getting all canvas info
 import requests
 
-
 ACCESS_TOKEN = '7~fxv2FzexxnfWEu2MzAYVnxlzRVvDFZnvzqhTppuhMO8i8xdZN0THcq5yZfSJmHL3'
-
 
 def getClasses():
     auth = requests.get(f"https://canvas.instructure.com/api/v1/courses?access_token={ACCESS_TOKEN}")
@@ -14,17 +12,8 @@ def get_enrollment(course_id: int):
 
 
 def getAssignments(user_id: int, course_id: int):
-    assignments = requests.get(f"https://canvas.instructure.com/api/v1/users/{user_id}}/courses/{course_id}/assignments")
-    return assignments.json()
+    return requests.get(f"https://canvas.instructure.com/api/v1/users/{user_id}/courses/{course_id}/assignments").json()
 
 def getQuizzes(course_id: int):
     return requests.get(f"https://canvas.instructure.com/api/v1/courses/{course_id}/quizzes").json()
 
-
-
-
-
-
-
-if __name__ == '__main__':
-    getClasses()
