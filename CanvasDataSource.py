eea4cd3782f0863af2c126fc6e50f01fdd7e83c9
Ryan Moazzami <ryan.moazz@gmail.com>
2019-10-26 13:38:53 -0400

    Merge branch 'master' of /Users/RyanMoazzami/PycharmProjects/HackGT6 with conflicts.

--- CanvasDataSource.py
+++ CanvasDataSource.py
@@ -1,7 +1,9 @@
 ## Getting all canvas info
 import requests
 
+
 ACCESS_TOKEN = '7~fxv2FzexxnfWEu2MzAYVnxlzRVvDFZnvzqhTppuhMO8i8xdZN0THcq5yZfSJmHL3'
+
 
 def getClasses():
     auth = requests.get(f"https://canvas.instructure.com/api/v1/courses?access_token={ACCESS_TOKEN}")
@@ -10,9 +12,19 @@ def getClasses():
 def get_enrollment(course_id: int):
     return requests.get(f'https://canvas.instructure.com/api/v1/courses/{course_id}/enrollments?access_token={ACCESS_TOKEN}').json()
 
+
 def getAssignments(user_id: int, course_id: int):
-    return requests.get(f"https://canvas.instructure.com/api/v1/users/{user_id}/courses/{course_id}/assignments?access_token={ACCESS_TOKEN}").json()
+    assignments = requests.get(f"https://canvas.instructure.com/api/v1/users/{user_id}/courses/{course_id}/assignments")
+    return assignments.json()
 
 def getQuizzes(course_id: int):
-    return requests.get(f"https://canvas.instructure.com/api/v1/courses/{course_id}/quizzes?access_token={ACCESS_TOKEN}").json()
+    return requests.get(f"https://canvas.instructure.com/api/v1/courses/{course_id}/quizzes").json()
 
+def getAnnouncements(course)
+
+
+
+
+
+if __name__ == '__main__':
+    getClasses()
